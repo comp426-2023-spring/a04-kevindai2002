ed042a9f1c1856f2dbca78c1304ed80dffea89ed
Kevin Dai <kevindai@email.unc.edu>
2023-05-08 13:27:23 -0400

    sub5

--- server.js
+++ server.js
@@ -2,13 +2,21 @@
 
 import minimist from "minimist";
 import express from 'express';
-import { play_game } from "./lib/rpsls.js";
+import { play_game } from "../lib/rpsls.js";
 
 const port = minimist(process.argv.slice(2)).port || 5000;
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+
+const rps_choices = ["rock", "paper", "scissors"];
+const rpsls_choices = ["rock", "paper", "scissors", "lizard", "spock"];
+
+/*var choice = args._[0];
+console.log(JSON.stringify(play_game(rpsls_choices, choice))); // put player choice in the rps function
+process.exit(0); // exit successfully
+*/
 
 //Get the root endpoint of your app
 app.get('/app', function(req, res) {
@@ -17,42 +25,42 @@ app.get('/app', function(req, res) {
 
 //Play RPS
 app.get('/app/rps', function(req, res) {
-    res.status(200).send(play_game()).end();
+    res.status(200).send(JSON.stringify(play_game(rps_choices,req)));
 });
 
 //Play RPSLS
 app.get('/app/rpsls', function(req, res) {
-    res.status(200).send(play_game());
+    res.status(200).send(JSON.stringify(play_game(rpsls_choices,req)));
 });
 
 //Play RPS against an opponent (URLEncoded data body)
 app.get('/app/rps/play', function(req, res) {
-    res.status(200).send(play_game(req.query.shot));
+    res.status(200).send(JSON.stringify(play_game(rps_choices,req.query.shot)));
 });
 
 //Play RPSLS against an opponent (URLEncoded data body)
 app.get('/app/rpsls/play', function(req, res) {
-    res.status(200).send(play_game(req.query.shot));
+    res.status(200).send(JSON.stringify(play_game(rpsls_choices,req.query.shot)));
 });
 
 //Play RPS against an opponent (JSON data body)
 app.post('/app/rps/play', function(req, res) {
-    res.status(200).send(play_game(req.body.shot));
+    res.status(200).send(JSON.stringify(play_game(rps_choices,req.body.shot)));
 });
 
 //Play RPSLS against an opponent (JSON data body)
 app.post('/app/rpsls/play', function(req, res) {
-    res.status(200).send(play_game(req.body.shot));
+    res.status(200).send(JSON.stringify(play_game(rpsls_choices,req.body.shot)));
 });
 
 //Play RPS against an opponent (parameter endpoint)
 app.get('/app/rps/play/:shot', function(req, res) {
-    res.status(200).send(play_game(req.params.shot));
+    res.status(200).send(JSON.stringify(play_game(rps_choices,req.params.shot)));
 });
 
 //Play RPSLS against an opponent (parameter endpoint)
 app.get('/app/rpsls/play/:shot', function(req, res) {
-    res.status(200).send(play_game(req.params.shot));
+    res.status(200).send(JSON.stringify(play_game(rpsls_choices,req.params.shot)));
 });
 
 //Call a nonexistent endpoint
