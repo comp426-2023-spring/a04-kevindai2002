ca3637fec99d55f80075b0b0d9898ce8a2952258
Kevin Dai <kevindai@email.unc.edu>
2023-05-08 12:57:40 -0400

    sub2

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 
 import minimist from "minimist";
 import express from 'express';
-import { play_game } from "./lib/rpsls";
+import { play_game } from "../lib/rpsls";
 
 const port = minimist(process.argv.slice(2)).port || 5000;
 const app = express();
